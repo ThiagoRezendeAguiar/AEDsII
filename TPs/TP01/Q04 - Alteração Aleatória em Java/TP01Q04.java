8e02a9428ff209e9e5088a4993b5c7e25c05a121
Thiago Rezende Aguiar <thiagorezende11@yahoo.com.br>
2023-09-04 16:32:32 -0300

    ultimos exercicios do tp01  adicionados

--- TPs/TP01/Q04 - Alteração Aleatória em Java/TP01Q04.java
+++ TPs/TP01/Q04 - Alteração Aleatória em Java/TP01Q04.java
@@ -1,41 +1,39 @@
 import java.util.Random;
 
-class TP01Q04{
-  public static String alteracao(String texto, Random gerador){
-    char caracter = (char)('a' + (Math.abs(gerador.nextInt()) % 26));
-    char caracterTrocar = (char)('a' + (Math.abs(gerador.nextInt()) % 26));
-    int n = texto.length();
-    String aleatorio =  new String();
-    
-    for(int i = 0; i < n; i++){
-      if(texto.charAt(i) == caracter){
+class TP01Q04 {
+  public static String alteracao(String texto, char caracter, char caracterTrocar) {
+    String aleatorio = "";
+
+    for (int i = 0; i < texto.length(); i++) {
+      if (texto.charAt(i) == caracter) {
         aleatorio += caracterTrocar;
+      } else {
+        aleatorio += texto.charAt(i);
       }
-      else{
-         aleatorio += texto.charAt(i);
-      }
-    } 
+    }
 
     return aleatorio;
-  } 
-  
-  public static void main(String[] args){
+  }
+
+  public static void main(String[] args) {
     Random gerador = new Random();
     gerador.setSeed(4);
-    
+
     String texto = MyIO.readLine();
-    
-    while(stop(texto)){
-      MyIO.println(alteracao(texto,gerador));
+
+    while (stop(texto)) {
+      char caracter = (char) ('a' + (Math.abs(gerador.nextInt()) % 26));
+      char caracterTrocar = (char) ('a' + (Math.abs(gerador.nextInt()) % 26));
+      MyIO.println(alteracao(texto, caracter, caracterTrocar));
       texto = MyIO.readLine();
     }
   }
 
- public static boolean stop(String texto){
-           boolean resp = true;
-           if (texto.charAt(0) == 'F' && texto.charAt(1) == 'I' && texto.charAt(2) == 'M'){
-                resp = false;
-           }
-           return resp;
+  public static boolean stop(String texto) {
+    boolean resp = true;
+    if (texto.charAt(0) == 'F' && texto.charAt(1) == 'I' && texto.charAt(2) == 'M') {
+      resp = false;
     }
+    return resp;
+  }
 }
